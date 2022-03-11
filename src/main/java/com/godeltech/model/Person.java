200cf2bf7e55b131df3daaedd45b1c89c8127ef6
y.bakhar <egorbahar@gmail.com>
2022-03-11 01:46:59 -0800

    L01-PT2:Refactor code

--- src/main/java/com/godeltech/model/Person.java
+++ src/main/java/com/godeltech/model/Person.java
@@ -1,7 +1,11 @@
 package com.godeltech.model;
 
 import com.godeltech.annotation.InjectRandomValue;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Person {
     @InjectRandomValue
     private String name;
@@ -9,28 +13,4 @@ public class Person {
     private Boolean isMarriage;
     @InjectRandomValue
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getMarriage() {
-        return isMarriage;
-    }
-
-    public void setMarriage(Boolean marriage) {
-        isMarriage = marriage;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
