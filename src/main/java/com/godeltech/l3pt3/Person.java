5bbe27daec9306858af5dd4db4165db743827e8e
y.bakhar <egorbahar@gmail.com>
2022-03-17 01:19:46 -0700

    L03-PT3:Refactor Person.java

--- src/main/java/com/godeltech/l3pt3/Person.java
+++ src/main/java/com/godeltech/l3pt3/Person.java
@@ -2,10 +2,8 @@ package com.godeltech.l3pt3;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 class Person {
     private final String name;
